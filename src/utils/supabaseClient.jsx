7608b7a4b0ea97e053c7b22a8953b444c3964386
BaktiyarYerkinov <94916523+BaktiyarYerkinov@users.noreply.github.com>
2024-02-29 16:53:06 +0500

    Changed title name

--- src/utils/supabaseClient.jsx
+++ src/utils/supabaseClient.jsx
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseUrl = "https://zqomegotkigtwjvfhcfz.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpxb21lZ290a2lndHdqdmZoY2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg0MjMyNzksImV4cCI6MjAyMzk5OTI3OX0.jf28ejku1nqSj3aZKLXqQwOcvoLnH2ALwR30Rpa3HU4";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
